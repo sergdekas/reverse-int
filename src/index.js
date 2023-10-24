e67125c41941fb340ab26da00b90813dfdbb5c0e
sergdekas <webvakula@gmail.com>
2023-10-24 15:48:56 +0300

    feat: add solved task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let result = "";
+    let str = n.toString();
+
+    for (let i = str.length - 1; i >= 0; i--) {
+        result += (str[i] != "-") ? str[i] : "";
+    }
+
+    while (result[0] == "0") result = result.slice(1);
+    return result;
 }
